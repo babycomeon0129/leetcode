a9ae6a1ae78fae117e1eca169ab632e1e13cc14e
babycomeon0129 <48780224+babycomeon0129@users.noreply.github.com>
2020-02-25 16:41:01 +0800

    Create 1351.js

---
+++ 1351. Count Negative Numbers in a Sorted Matrix/1351.js
@@ -0,0 +1,18 @@
+var countNegatives = function(grid) {
+    let negative = 0;
+    
+    for(let i = 0 ; i < grid.length ; i++){
+        
+        for(let j = 0 ; j < grid[i].length ; j++){
+            
+            if(grid[i][j] < 0 ){
+                negative++;
+            }
+        }
+        
+    }
+    
+    
+    return negative;
+        
+};
